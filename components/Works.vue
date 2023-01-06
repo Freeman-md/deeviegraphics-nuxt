6932010074b79e247c2a493c9fc4ece932ab28a4
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2023-01-06 09:03:26 +0000

    UPDATE: update works section

--- components/Works.vue
+++ components/Works.vue
@@ -1,12 +1,32 @@
 <template>
     <div class="h-full my-20 py-32 responsive-container" id="works">
         <div class="flex flex-col space-y-6">
-            <h3 class="text-6xl font-black" >My works</h3>
+            <h3 class="text-6xl font-black">My works</h3>
             <span>Here are some of the cool projects I've worked on</span>
 
             <div>
                 <div :class="`grid grid-cols-3`">
-                    <span v-for="(tab, index) in tabs" :key="index" class="py-4 hover:text-white text-center text-xs sm:text-base border-b-2 cursor-pointer transition duration-200 hover:border-white" @click.prevent="selectTab(tab)" :class="selectedTab && selectedTab.index === tab.index ? 'text-white border-white' : 'text-gray-600 border-gray-600'">
+                    <span
+                        v-for="(tab, index) in tabs"
+                        :key="index"
+                        class="
+                            py-4
+                            hover:text-white
+                            text-center text-xs
+                            sm:text-base
+                            border-b-2
+                            cursor-pointer
+                            transition
+                            duration-200
+                            hover:border-white
+                        "
+                        @click.prevent="selectTab(tab)"
+                        :class="
+                            selectedTab && selectedTab.index === tab.index
+                                ? 'text-white border-white'
+                                : 'text-gray-600 border-gray-600'
+                        "
+                    >
                         {{ tab.title }}
                     </span>
                 </div>
@@ -21,25 +41,32 @@
 </template>
 
 <script>
-import { reactive, ref } from '@vue/composition-api'
+import { reactive, ref } from "@vue/composition-api";
 export default {
     setup() {
         const tabs = reactive([
-             
+            {
+                index: 1,
+                title: "Flyer Designs",
+            },
+            {
+                index: 2,
+                title: "Logo Designs",
+            },
             {
                 index: 3,
-                title: 'Videos'
-            }
-        ])
-        const selectedTab = ref(tabs[0])
+                title: "Videos",
+            },
+        ]);
+        const selectedTab = ref(tabs[0]);
 
-        const selectTab = (tab) => selectedTab.value = tab
+        const selectTab = (tab) => (selectedTab.value = tab);
 
         return {
             selectedTab,
             tabs,
-            selectTab
-        }
-    }
-}
+            selectTab,
+        };
+    },
+};
 </script>
